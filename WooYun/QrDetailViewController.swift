eb366bb89c75f72fb629b95a66ec1ebd4fdbe4e5
pandazheng <543828315@qq.com>
2015-06-22 14:30:03 +0800

    乌云漏洞平台
    乌云漏洞平台查询

---
+++ WooYun/QrDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  QrDetailViewController.swift
+//  WooYun
+//
+//  Created by panda zheng on 15/6/22.
+//  Copyright (c) 2015年 panda zheng. All rights reserved.
+//
+
+import UIKit
+
+class QrDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "漏洞详情"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WooYun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WooYun
+//
+//  Created by panda zheng on 15/6/22.
+//  Copyright (c) 2015年 panda zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
